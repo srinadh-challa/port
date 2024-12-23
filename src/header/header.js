3864734582157dcfd64b217a1adf81ddbe24ec98
srinadh-challa <srinadhchallaa@gmail.com>
2024-12-23 16:33:29 +0530

    first commit

---
+++ src/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello, World!</h1>
+      <p>Welcome to my first React app!</p>
+    </div>
+  );
+}
+
+export default App;
